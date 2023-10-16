029a257bb75adc8a86fb6bbafaff3a31192cc3d6
Ankit Kushwaha <145086869+Ankit-K0@users.noreply.github.com>
2023-10-16 12:38:20 +0530

    Create  Python-Program-15.py

---
+++ Python-Program-15.py
@@ -0,0 +1,16 @@
+#print date and time
+
+#import date and time from datetime module
+from datetime import datetime
+
+#declare a variable for manual year,month and date
+a = datetime(2023,11,20)
+
+#print a
+print(a)
+
+#declare a variable for manual year,month,date and time 
+b = datetime(2023,11,21,8,35,59,455465)
+
+#print b
+print(b)
