fa99fc078617dcc94576f0231c70cb46cfce79fb
Ankit Kushwaha <145086869+Ankit-K0@users.noreply.github.com>
2023-10-24 12:31:36 +0530

    Update Python-Program-8.py

--- Python-Program-8.py
+++ Python-Program-8.py
@@ -1,7 +1,5 @@
-#Pyramid using python
+#type casting 
 
-n = 5
-for i in range(0, n):
-    for j in range(0, i+1):
-        print("*", end=" ")
-    print()
+price = 25 
+item = "apple" 
+print("cost of %s is %d "%(item,price))
